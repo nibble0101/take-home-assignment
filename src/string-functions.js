e5745d421f767aeea35768590b6449aed1bb1c4f
nibble0101 <mj.mawa@yahoo.com>
2021-03-10 11:00:40 +0300

    Create file for declaring utility functions and made outline of task requirements

---
+++ src/string-functions.js
@@ -0,0 +1,8 @@
+/* 
+- Strip input text of leading or trailing white spaces. This will catch empty strings
+- Split text into array of paragraphs. Successive paragraphs are separated by at least two successive line breaks
+- Replace new line characters and at least two successive white spaces in each paragraph by a single white space
+- Split paragraphs into array of lines
+- Join array of lines back to paragraph. Separate successive lines by a new line character
+- Join paragraphs to string of text. Successive paragraphs are separated by double new line characters
+*/
